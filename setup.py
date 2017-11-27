9c81ebaaf09e46ab41dd74972eaa3be52a6283cb
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2017-11-27 18:17:46 +0100

    Re #2: Added MANIFEST.in and improved setup.py to include omr_models. Requirements now contain everything that is *directly* required by sheet_manager, ignoring at the moment  the implied dependencies of `omr` and `lasagne_wrapper`.

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ from setuptools import setup
 setup(
     name='sheet_manager',
     version='0.1',
-    packages=['', 'sheet_manager', 'sheet_manager.gui', 'sheet_manager.omr', 'sheet_manager.omr.utils',
-              'sheet_manager.omr.config', 'sheet_manager.omr.models'],
+    packages=['',
+              'sheet_manager',
+              'sheet_manager.gui'],
+    include_package_data=True,
     url='',
     license='(c) All rights reserved.',
     author='Matthias Dorfer',
